983c4b3b20d13a0df0c1a53c39cfa5cfebc3f87b
rickluiz <rickluizms@gmail.com>
2023-06-18 20:23:19 -0300

    update commit

--- src/main/java/com/skins/restapi/model/Skin.java
+++ src/main/java/com/skins/restapi/model/Skin.java
@@ -13,44 +13,88 @@ public class Skin {
     private Long id;
     @Column(nullable = false)
     private String nome;
-    @Column(nullable = false)
+    private String img;
+    private String dispon;
     private String fase;
-    @Column(nullable = false)
+    private String old_price;
     private String price;
-    @Column(nullable = false)
+    private String discount;
     private String fn;
 
     public Skin() {}
-    
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDispon() {
+        return dispon;
+    }
+
+    public void setDispon(String dispon) {
+        this.dispon = dispon;
+    }
+
     public String getFase() {
         return fase;
     }
+
     public void setFase(String fase) {
         this.fase = fase;
     }
+
+    public String getOld_price() {
+        return old_price;
+    }
+
+    public void setOld_price(String old_price) {
+        this.old_price = old_price;
+    }
+
     public String getPrice() {
         return price;
     }
+
     public void setPrice(String price) {
         this.price = price;
     }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
     public String getFn() {
         return fn;
     }
+
     public void setFn(String fn) {
         this.fn = fn;
     }
+
+    
     
 }
